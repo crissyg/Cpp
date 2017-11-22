72cafdaed5e3e98a317af699245b2d89ff63f104
Christina Gordon <crissyg@users.noreply.github.com>
2017-11-22 11:52:52 -0500

    Created dataTypes.cpp
    C++ example with different data types and formatted output

---
+++ dataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // C++ example with different data types and formatted output
+    int i;
+    char c;
+    double d;
+    long l;
+    float f;
+    scanf("%d %ld %c %f %lf", &i, &l,&c,&f,&d);
+    printf("%d \n%ld \n%c \n%.3f \n%.9lf", i, l,c,f,d);
+ 
+
+
+    return 0;
+}
